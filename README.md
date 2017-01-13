1d6dc56be318cd12d36dc99d12d7e7a0c8b42a37
Oystein Amundsen <oystein.amundsen@bouvet.no>
2017-01-13 15:07:34 +0100

    Upgraded angular-cli

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Fint Shared Components #
 
-This project was generated with [angular-cli](https://github.com/angular/angular-cli) version 1.0.0-beta.24.
+This project was generated with [angular-cli](https://github.com/angular/angular-cli) version 1.0.0-beta.25.5.
 
 ## Included in this package ##
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
 
 import { LibSharedModule } from './shared/shared.module';
 import { EventModule } from './events/events.module';
 
 @NgModule({
+  declarations: [],
   imports: [
     BrowserModule,
-    HttpModule,
     FormsModule,
+    HttpModule,
     MaterialModule, //.forRoot(),
 
     EventModule,
     LibSharedModule
   ],
-  declarations: [
-  ],
-  exports: [
-    EventModule,
-    LibSharedModule
-  ]
+  exports: [EventModule, LibSharedModule],
+  providers: []
 })
 export class LibModule { }
